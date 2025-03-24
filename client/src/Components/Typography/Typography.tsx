9a0a0f9ca176e22e3e10c7d9fe975dcb520b1388
Дмитрий Беликов <dduim@bk.ru>
2025-03-24 18:07:00 +0300

    Change Typography

--- client/src/Components/Typography/Typography.tsx
+++ client/src/Components/Typography/Typography.tsx
@@ -1,10 +1,17 @@
-type TTypography = {
-  tag: "div" | "h1" | "h2";
+import { HTMLProps } from "react";
+import styles from "./Typography.module.scss";
+
+type TTypography = HTMLProps<HTMLDivElement> & {
+  tag?: "div" | "h1" | "h2" | "p" | "span";
 };
 
-const Typography = ({ tag } : TTypography) => {
+const FONT = {
+  base700: "",
+} as const;
+
+const Typography = ({ tag = "p", children }: TTypography) => {
   const Component = tag;
-  return <Component>Typography</Component>;
+  return <Component className={`${styles.qwe}`}>{children}</Component>;
 };
 
 export default Typography;
