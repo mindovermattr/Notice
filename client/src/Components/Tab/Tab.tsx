56a0598d8e953cb2ce2168af27e057d6f97ae366
mindovermattr <dduim@bk.ru>
2025-05-03 17:39:09 +0300

    Добавил эффект наведения для кнопки, изменил структуру табов

--- client/src/Components/Tab/Tab.tsx
+++ client/src/Components/Tab/Tab.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
 import { HTMLProps } from "react";
+import Button from "../Button/Button";
 import styles from "./Tab.module.scss";
 type TTab = HTMLProps<HTMLElement>;
 
@@ -10,33 +11,48 @@ const Tab = ({ className, ...props }: TTab) => {
 
   return (
     <header className={`${styles.tab} ${className}`} {...props}>
-      <Link
-        href={"Tasklist"}
-        className={`${styles.tab__option} ${
-          selectedSegment === "Tasklist" ? styles["tab__option--selected"] : ""
-        }`}
-      >
-        <Image width={24} height={24} src="/icons/list.svg" alt="board-icon" />
-        <p className={styles.tab__text}>Task List</p>
-      </Link>
-      <Link
-        href={"Kanban"}
-        className={`${styles.tab__option} ${
-          selectedSegment === "Kanban" ? styles["tab__option--selected"] : ""
-        }`}
-      >
-        <Image
-          width={24}
-          height={24}
-          src="/icons/dashboard.svg"
-          alt="board-icon"
-        />
-        <p className={styles.tab__text}>Boards</p>
-      </Link>
-      <Link href={"Gantt"} className={styles.tab__option}>
-        <Image width={24} height={24} src="/icons/gantt.svg" alt="board-icon" />
-        <p className={styles.tab__text}>Gantt</p>
-      </Link>
+      <div className={styles.tab__links}>
+        <Link
+          href={"Tasklist"}
+          className={`${styles.tab__option} ${
+            selectedSegment === "Tasklist"
+              ? styles["tab__option--selected"]
+              : ""
+          }`}
+        >
+          <Image
+            width={24}
+            height={24}
+            src="/icons/list.svg"
+            alt="board-icon"
+          />
+          <p className={styles.tab__text}>Task List</p>
+        </Link>
+        <Link
+          href={"Kanban"}
+          className={`${styles.tab__option} ${
+            selectedSegment === "Kanban" ? styles["tab__option--selected"] : ""
+          }`}
+        >
+          <Image
+            width={24}
+            height={24}
+            src="/icons/dashboard.svg"
+            alt="board-icon"
+          />
+          <p className={styles.tab__text}>Boards</p>
+        </Link>
+        <Link href={"Gantt"} className={styles.tab__option}>
+          <Image
+            width={24}
+            height={24}
+            src="/icons/gantt.svg"
+            alt="board-icon"
+          />
+          <p className={styles.tab__text}>Gantt</p>
+        </Link>
+      </div>
+     {selectedSegment === "Tasklist" && <Button>Создать</Button>}
     </header>
   );
 };
